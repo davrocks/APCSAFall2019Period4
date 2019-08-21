5c0bdba496212aefde6e4a824be2cd42f89b333d
APCS2nd <APCS2nd@MHS-LT9-17>
2019-08-21 12:16:54 -0700

    complete Hello World Project

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,12 @@
+//Davin Singh
+//August 19, 2019
+//First class (iconic) in java for APCS A
+//Prints "Hello, World!" to the screen
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
