0e266818a351a7b86c85dc5bcc4ed9abb9074eb6
APCS2nd <APCS2nd@MHS-LT9-17>
2019-09-04 12:03:20 -0700

    Update HelloWorld

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -6,7 +6,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
+		System.out.println("\"Quotes\"");
+		System.out.println("Slashes \\//");
+		System.out.println("How '\"confounding' \"\\\" it is!");	}
 
 }
