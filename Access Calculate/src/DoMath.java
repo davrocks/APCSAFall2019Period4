73edc19b5a6a2b7898d5a0a5d983a9523e1a0339
APCS2nd <APCS2nd@MHS-LT9-17>
2019-09-04 12:04:29 -0700

    Added Access Calculate

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,15 @@
+/* Davin Singh
+ * APCSA 4th period
+ * August 29, 2019
+ * Call math methods from Calculate library
+ * Client code or runner code
+ */
+public class DoMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println(Calculate.square(12));
+			System.out.println(Calculate.cube(3));
+	}
+
+}
