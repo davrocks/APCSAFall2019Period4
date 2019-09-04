284ae83b9ad02ddad346a92665edabd8fa535324
APCS2nd <APCS2nd@MHS-LT9-17>
2019-09-04 12:02:29 -0700

    Added Calculate Library

---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,32 @@
+/* Davin Singh
+ * August 29, 2019
+ * AP CSA 4th period
+ * group of methods that perform math calculations (a library)
+ */
+public class Calculate {
+	//A call to square returns the square of the value passed.
+	//It accepts and returns an integer.
+	public static int square(int number) {
+		int answer = 0;
+		answer = number*number;
+		return answer;
+	}
+	
+	//A call to cube returns the cube of the value passed.
+	//It accepts and returns an integer.
+	public static int cube(int number) {
+		int answer = 0;
+		answer = number*number*number;
+		return answer;		
+	}
+	
+	//A call to cube returns the cube of the value passed.
+	//It accepts and returns an integer.
+	public static int average(int number) {
+		int answer = 0;
+		answer = number*number*number;
+		return answer;
+				
+	}
+
+}
