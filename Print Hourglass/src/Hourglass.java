6d09ffa9e05004db8a43dc53b3ca9d8af877e112
Vinay Singh <xcamp15@gmail.com>
2019-10-09 17:16:10 -0700

    added a comment on # of lines

--- Print Hourglass/src/Hourglass.java
+++ Print Hourglass/src/Hourglass.java
@@ -54,7 +54,7 @@ public class Hourglass {
 			enter();
 		}
 		space(size/2);
-		line(size - 2*(size/2-1));
+		line(size - 2*(size/2-1)); //the number of lines is equal to the number of colons on the previous row
 		enter();
 		for (int line = size/2 - 1; line >=1; line--) {
 			space(line);
